2cd14f1ed9014d0dce884739c4ef16fa3cd103d8
bsokat <barissokat@gmail.com>
2017-03-02 09:47:57 +0300

    Create Custom Helper with Extension Method

--- CreateCustomHelper/Helpers/CustomHelper.cs
+++ CreateCustomHelper/Helpers/CustomHelper.cs
@@ -16,5 +16,14 @@ namespace CreateCustomHelper.Helpers
             MvcHtmlString textBox = MvcHtmlString.Create(inputHtml);
             return textBox;
         }
+
+        public static MvcHtmlString TextBox2(this HtmlHelper helper, string id, string name = "")
+        {
+            if (string.IsNullOrEmpty(name))
+                name = id;
+            string inputHtml = string.Format("<input type='text' id='{0}' name='{1}' />", id, name);
+            MvcHtmlString textBox = MvcHtmlString.Create(inputHtml);
+            return textBox;
+        }
     }
 }
