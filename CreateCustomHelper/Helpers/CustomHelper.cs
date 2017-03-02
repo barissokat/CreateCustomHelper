07ecd62d609d9d3dc2cb36dfeeb82ef592092e3a
bsokat <barissokat@gmail.com>
2017-03-02 09:40:51 +0300

    Create Custom Helper with custom class and static method

---
+++ CreateCustomHelper/Helpers/CustomHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CreateCustomHelper.Helpers
+{
+    public static class CustomHelper
+    {
+        public static MvcHtmlString TextBox(string id, string name = "")
+        {
+            if (string.IsNullOrEmpty(name))
+                name = id;
+            string inputHtml = string.Format("<input type='text' id='{0}' name='{1}' />", id, name);
+            MvcHtmlString textBox = MvcHtmlString.Create(inputHtml);
+            return textBox;
+        }
+    }
+}
